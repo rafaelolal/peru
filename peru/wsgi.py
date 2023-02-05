88890a6177507a640007ca380d8bdfb9097f3ac7
Rafael Almeida <rafaelpbcp@gmail.com>
2023-02-05 14:28:10 -0500

    idek cuzzo

--- peru/wsgi.py
+++ peru/wsgi.py
@@ -7,10 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
-from django.core.wsgi import get_wsgi_application
+# add the virtualenv site-packages path to the sys.path
+site_packages = '/home/rafaelolal/envs/peru_env/lib/python3.10/site-packages'
+if site_packages not in sys.path:
+    sys.path.append(site_packages)
+
+project_path = '/home/rafaelolal/peru'
+project_path2 = '/home/rafaelolal/peru/peru'
+
+if project_path not in sys.path:
+    sys.path.append(project_path)
+if project_path2 not in sys.path:
+    sys.path.append(project_path2)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'peru.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
